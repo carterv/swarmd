aef9b5fc589efa7ea4e6a3242595a0b9e4bf3d3b
Carter Van Deuren <carter.van.deuren@gmail.com>
2018-02-13 18:19:31 -0800

    Add filepartrequest header

---
+++ packets/filepartrequest.go
@@ -0,0 +1,52 @@
+package packets
+
+import (
+	"fmt"
+	"encoding/binary"
+	"encoding/hex"
+)
+
+type FilePartRequestHeader struct {
+	Common     CommonHeader
+	FileHash     [16]uint8
+	PartNumber uint16
+}
+
+func (h *FilePartRequestHeader) Initialize(FileHash [16]uint8, PartNumber uint16) {
+	h.FileHash = FileHash
+	h.PartNumber = PartNumber
+
+	h.Common.Initialize(uint16(CommonHeaderSize)+18, h.PacketType())
+}
+
+func (h *FilePartRequestHeader) Serialize() SerializedPacket {
+	raw := make(SerializedPacket, h.Common.PacketLength)
+
+	copy(raw[:CommonHeaderSize], h.Common.Serialize())
+	copy(raw[CommonHeaderSize:CommonHeaderSize+16], h.FileHash[:])
+	binary.BigEndian.PutUint16(raw[CommonHeaderSize+16:h.Common.PacketLength], h.PartNumber)
+
+	raw.CalculateChecksum()
+
+	return raw
+}
+
+func (h *FilePartRequestHeader) Deserialize(raw SerializedPacket) bool {
+	if !h.Common.Deserialize(raw) {
+		return false
+	}
+
+	copy(h.FileHash[:], raw[CommonHeaderSize:CommonHeaderSize+16])
+	h.PartNumber = binary.BigEndian.Uint16(raw[CommonHeaderSize+16:h.Common.PacketLength])
+
+	return true
+}
+
+func (h *FilePartRequestHeader) ToString() string {
+	return fmt.Sprintf("%sFile Hash: %s\nPartNumber: %d\n", h.Common.ToString(),
+		hex.Dump(h.FileHash[:]), h.PartNumber)
+}
+
+func (h *FilePartRequestHeader) PacketType() uint8 {
+	return 5
+}
